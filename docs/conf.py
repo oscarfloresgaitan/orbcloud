d65b795ae4722315dee53ad105716e61782b5c28
Oscar A. Flores Gaitán <ofg@udel.edu>
2026-06-25 16:42:09 -0700

    Bump version to 0.1.2 for Read the Docs configuration update

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ author = 'Oscar Flores Gaitán and Sam Hopper'
 root_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
